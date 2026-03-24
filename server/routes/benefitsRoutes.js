920c75a2a5e01ea391ab253754d6c371d04d8b24
millerishi8-art <millerishi8@gmail.com>
2026-03-24 12:14:00 +0200

    Force fix file path and casing for iso3166

--- server/routes/benefitsRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getBenefits } from '../controllers/benefitsController.js';
-
-const router = Router();
-
-router.get('/', getBenefits);
-
-export default router;
--- server/start-server.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting server on http://localhost:5000 ...
-node --watch server.js
-pause
